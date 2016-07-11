68e531d943141755949a3cfe41724bf782f68bf7
Mukesh Chandra <Mukesh Chandra>
2016-07-11 16:58:28 +0530

    solved location problem

--- app/src/main/java/com/coloredcow/myapplication/AdminDashBoard.java
+++ app/src/main/java/com/coloredcow/myapplication/AdminDashBoard.java
@@ -1,6 +1,8 @@
 package com.coloredcow.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -18,7 +20,19 @@ public class AdminDashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dash_board);
 // add branch activity
-
+        Button c=(Button)findViewById(R.id.logout);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences mPrefs = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = mPrefs.edit();
+                editor.putString("_user_name", "");
+                editor.putString("_pass_", "");
+                editor.putBoolean("my_first_time",true);
+                editor.commit();
+                startActivity(new Intent(AdminDashBoard.this,Login.class));
+            }
+        });
 
     }
     @Override
---
+++ app/src/main/java/com/coloredcow/myapplication/GetSale.java
@@ -0,0 +1,7 @@
+package com.coloredcow.myapplication;
+
+/**
+ * Created by asd on 7/11/2016.
+ */
+public class GetSale {
+}
